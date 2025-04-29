2c6d361e589f2dce641dd77af97afc97bbdda183
Najih Mld <najihmaulida@gmail.com>
2025-04-29 20:14:51 +0700

    Initial app

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function DashboardPage() {
+    return (
+        <>
+        <h1>Hello Next.js! Dashboard Page</h1>
+        <div>
+            <Button>Button</Button>
+        </div>
+        </>
+    )
+  }
---
+++ src/components/layouts/Siderbar.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Image from 'next/image';
+
+const menuItems = [
+  {
+    label: 'Dashboard',
+  },
+  {
+    label: 'Users',
+  },
+  {
+    label: 'Settings',
+  },
+];
+
+function Siderbar() {
+  return (
+    <aside className="w-[220px]">
+      <div className="p-4">
+        <Image height={22} width={77} src={'/logo.svg'} alt="Sidebar Logo" />
+      </div>
+      <nav className="mt-4">
+        <ul className="space-y-2">
+          {menuItems.map((item) => (
+            <li key={item.label} className="p-2 hover:bg-gray-700">
+              {item.label}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+}
+
+export default Siderbar;
