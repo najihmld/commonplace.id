427a0078c6fe9402c93141c4dfdb01ae2cd57b22
Najih Mld <najihmaulida@gmail.com>
2025-06-26 08:03:00 +0700

    feat: add TestPage component with image gallery layout

---
+++ src/app/[locale]/(main)/test/page.tsx
@@ -0,0 +1,112 @@
+function TestPage() {
+  return (
+    <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+      <div className="grid gap-4">
+        <div>
+          12
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          11
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          10
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          9
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          8
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          7
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          6
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          5
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          4
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          3
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          2
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          1
+          <img
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TestPage;
