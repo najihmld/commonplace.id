b86dda78b1b9683096e3e5e7169384635c75e615
Najih Mld <najihmaulida@gmail.com>
2025-06-28 07:53:30 +0700

    fix: add suppressHydrationWarning attribute to body element in layout for improved hydration handling

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -37,6 +37,7 @@ export default async function RootLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NextIntlClientProvider>
